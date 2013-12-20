d8779865c3f5b8fe0b0627a86a0d35976bac6554
Adrien Candiotti <adrien.candiotti@gmail.com>
2013-12-19 19:58:33 -0800

    add - first populating the repo

---
+++ whyd2mp3.js
@@ -0,0 +1,34 @@
+/*
+** Whyd 2 mp3
+** A very simple script that adds links to different website to download a song
+** from the whyd website.
+*/
+
+(function(){
+    // 
+    function mrtzcmp3(title) {
+        title.replace(/\s+/g, '-').toLowerCase();
+        return ('http://mrtzcmp3.net/search/' + title);
+    }
+
+    function mnogomp3(title) {
+        return ('http://mnogomp3.ru/music/' + title);
+    }
+    
+    var websites = [mrtzcmp3, mnogomp3];
+
+    function    add_links(post) {
+        var track_title = $(".post").find($("h2")).find("a").html();
+        var btns = post.find($(".btns");
+
+        for (var i=0; i < websites.length; i++) {
+            var website_name = websites[i].name;
+            var url = websites[i](track_title);
+            var link = '<a href="' + url + '">' + website_name + '</a>';
+
+            btns.append($(link));
+        };
+    }
+
+    $('.post').each(add_links);
+})();
